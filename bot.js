84c0226dbdf05237cbd083bf4d5da8aecc7c0f1d
1Hamad1 <44902218+1Hamad1@users.noreply.github.com>
2018-11-12 13:45:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "473433339035123712"; // ايدي السررفر
+var channel = "507696823540908043";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**tt7da robo ha? **')
     },305);
 })
 
