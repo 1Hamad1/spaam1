d36ebbea62e569f8fd18ba1ccdab8d6ed8ac5b17
1Hamad1 <44902218+1Hamad1@users.noreply.github.com>
2018-11-12 13:56:24 +0300

    Update Dream.js

--- Dream.js
+++ Dream.js
@@ -4,11 +4,11 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("498233231922036756")
+let channel =     client.channels.get("473433339035123712")
 setInterval(function() {
-channel.send(`youtube`);
+channel.send(`tt7da y robo ha?  :) `);
 }, 25)
 })
  
  
-client.login('TOKEN');
+client.login('NTAyNTU2MTg5NjU4MTg1NzI5.DsrsKQ.LOxCjlrEfBwVw5AFnnEglzZeW1M');
